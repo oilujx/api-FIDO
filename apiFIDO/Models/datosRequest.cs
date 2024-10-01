c552da173ed3c87293011dd4d901aa86ebac58ed
Julio Juan Luna Yuman <julio.luna@distelsa.com.gt>
2024-09-30 21:28:34 -0600

    creacion de cliente

---
+++ apiFIDO/Models/datosRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace apiFIDO.Models
+{
+    public class datosRequest
+    {
+        #region datosCanje
+
+        public string? codigoCliente { get; set; }
+        public string? indCanjeado { get; set; }
+
+        #endregion
+
+        #region datosRegistro
+        public string? Nombre {  get; set; }
+        public string? Telefono { get; set; }
+        public string? Direccion { get; set; }
+        public string? Correo { get; set; }
+        public string? Id_Raza { get; set; }
+        public string? Peso_Perro { get; set; }
+
+        #endregion
+    }
+}
